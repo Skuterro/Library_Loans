70c7603f5dd1e8a35b55994771b605d3e8b742b8
Kacper <kacperkk129@gmail.com>
2025-08-24 20:35:16 +0200

    format

--- frontend/src/utils/formatDate.ts
+++ frontend/src/utils/formatDate.ts
@@ -6,7 +6,7 @@ export const formatDate = (dateStr: string | null) => {
   const day = String(date.getUTCDate()).padStart(2, "0");
   const month = String(date.getUTCMonth() + 1).padStart(2, "0");
   const year = date.getUTCFullYear();
-  const hours = String(date.getUTCHours()).padStart(2, "0");
+  const hours = String(date.getUTCHours() + 2).padStart(2, "0");
   const minutes = String(date.getUTCMinutes()).padStart(2, "0");
 
   return `${day}.${month}.${year} ${hours}:${minutes}`;
